8d649c789916dba245bea1e64a5a452d8106f919
x-147 <no-email>
2025-03-20 11:15:25 +0700

    feat: applying global font style, using metadata

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,22 @@
 import './globals.css'
+import { Overpass } from 'next/font/google'
+
+const overpass = Overpass({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+})
+
+export const metadata = {
+  title: 'Frontend Mentor | Interactive rating component',
+  description: '',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={overpass.className}>{children}</body>
     </html>
   )
 }
