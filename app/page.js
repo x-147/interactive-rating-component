27e1ebf4b51a7647832082148925df094f2d6102
x-147 <no-email>
2025-03-20 09:01:05 +0700

    feat: onclick handler

--- app/page.js
+++ app/page.js
@@ -1,4 +1,26 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function Home() {
+  const [selection, saveSelection] = useState([])
+
+  const handleOnClick = (e) => {
+    console.log('selection: ', selection)
+    const value = e.target.textContent
+    if (selection.includes(value)) {
+      const newSelection = selection.filter((val) => val !== value)
+      saveSelection(() => newSelection)
+    } else {
+      saveSelection(() => selection.concat(value))
+    }
+  }
+
+  const handleOnSubmit = () => {
+    console.log('selection: ', selection)
+    saveSelection([])
+  }
+
   return (
     <div className="container">
       <div className="star-container">
@@ -10,13 +32,23 @@ export default function Home() {
         appreciated to help us improve our offering!
       </p>
       <div className="round-button-container">
-        <div className="number-container">1</div>
-        <div className="number-container">2</div>
-        <div className="number-container">3</div>
-        <div className="number-container">4</div>
-        <div className="number-container">5</div>
+        <div className="number-container" onClick={handleOnClick}>
+          1
+        </div>
+        <div className="number-container" onClick={handleOnClick}>
+          2
+        </div>
+        <div className="number-container" onClick={handleOnClick}>
+          3
+        </div>
+        <div className="number-container" onClick={handleOnClick}>
+          4
+        </div>
+        <div className="number-container" onClick={handleOnClick}>
+          5
+        </div>
       </div>
-      <button>Submit</button>
+      <button onClick={handleOnSubmit}>Submit</button>
     </div>
   )
 }
